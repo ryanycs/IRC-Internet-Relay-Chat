8f76b53848c3182312308dc8d1e6935081fb52f3
Ryan.Y.C.Su <Ryan.Y.C.Su@gmail.com>
2024-03-29 22:40:27 +0800

    Add User class and refactor client handling

--- server.py
+++ server.py
@@ -5,11 +5,29 @@ import threading
 import time
 
 
+class User:
+    def __init__(
+        self,
+        conn: socket.socket,
+        addr: tuple,
+        username: str,
+    ):
+        self.conn = conn
+        self.username = username
+        self.addr = addr
+
+    def __str__(self):
+        return f"{self.username}\t{self.addr}\t['#public']"
+
+    def __repr__(self):
+        return self.__str__()
+
+
 class Server:
     def __init__(self, host: str, port: int):
         self.host = host
         self.port = port
-        self.clients = {}  # {addr: {"conn": conn, "username": None}}
+        self.users = []
         self.commands = {
             "/USER": self.command_user,
             "/WHO": self.command_who,
@@ -29,38 +47,35 @@ class Server:
         ).encode("utf-8")
         conn.send(data)
 
-    def command_user(self, conn: socket.socket, addr: tuple, message: str):
+    def command_user(self, user: User, message: str):
         """
         Extracts the username from the message and adds it to the list of clients
         """
         # extract username from message
         username = re.match("^\/USER\s*(.*)", message).group(1)
         # add user to list of users
-        self.clients[addr]["username"] = username
+        user.username = username
         # notify all connected clients that a new user has joined
-        for client_addr in self.clients:
-            if client_addr != addr:
-                self.send_message(
-                    self.clients[client_addr]["conn"],
-                    "-->",
-                    f"{username} has joined #public.",
-                )
+        for client in self.users:
+            self.send_message(
+                client.conn,
+                "-->",
+                f"{user.username} has joined #public.",
+            )
 
-    def command_who(self, conn: socket.socket, addr: tuple, message: str):
+    def command_who(self, user: User, message: str):
         """
         Sends a list of all connected clients to the client
         """
         tmpstr = []
-        for client in self.clients:
+        for client in self.users:
             # only show clients that have set a username
-            if self.clients[client]["username"] is not None:
-                tmpstr.append(
-                    f"{self.clients[client]['username']}\t{client}\t['#public']"
-                )
+            if client.username is not None:
+                tmpstr.append(f"{client.username}\t{client.addr}\t['#public']")
         message = "USER\tFROM\tCHANNEL\n" + "\n".join(tmpstr)
-        self.send_message(conn, None, message)
+        self.send_message(user.conn, None, message)
 
-    def command_quit(self, conn: socket.socket, addr: tuple, message: str):
+    def command_quit(self, user: User, message: str):
         """
         Closes the connection with the client
         """
@@ -68,60 +83,63 @@ class Server:
         message = match_obj.group(1) if match_obj else None
 
         # notify all connected clients that a user has left
-        for client_addr in self.clients:
-            if client_addr == addr:
+        for client in self.users:
+            if client == user:
                 continue
             if message:
                 # send quit message
-                self.send_message(self.clients[client_addr]["conn"], "-->", message)
+                self.send_message(client.conn, user.username, message)
             self.send_message(
-                self.clients[client_addr]["conn"],
+                client.conn,
                 "-->",
-                f"{self.clients[addr]['username']} has left #public.",
+                f"{user.username} has left #public.",
             )
 
-        conn.close()
-        self.clients.pop(addr)
+        user.conn.close()
+        try:
+            self.users.remove(user)
+        except ValueError:
+            pass
 
-    def handle_client(self, conn: socket.socket, addr: tuple):
+    def handle_client(self, user: User):
         """
         Handles a new client connection
         """
-        print(f"New connection from {addr}")
+        print(f"New connection from {user.addr}")
 
         while True:
             # check if connection is still open
-            if conn.fileno() == -1:
+            if user.conn.fileno() == -1:
                 break
 
-            data = conn.recv(1024)
+            data = user.conn.recv(1024)
             if not data:
                 break
             data = json.loads(data.decode("utf-8"))
             message = data["message"]
-            print(f"from {addr}, msg = {message}")
+            print(f"from {user.addr}, msg = {message}")
 
             match_obj = re.match("/[A-Z]+", message)
             command = match_obj.group() if match_obj else None
             if command in self.commands.keys():
                 # execute command
-                self.commands[command](conn, addr, message)
+                self.commands[command](user, message)
             else:
                 # check if user has set a username
-                if self.clients[addr]["username"] is None:
-                    self.send_message(conn, "-->", "Please set a username first.")
+                if user.username is None:
+                    self.send_message(user.conn, "-->", "Please set a username first.")
                     continue
                 # broadcast message to all connected clients
-                for client_addr in self.clients:
-                    if client_addr != addr:
+                for client in self.users:
+                    if client != user:
                         self.send_message(
-                            self.clients[client_addr]["conn"],
-                            self.clients[addr]["username"],
+                            client.conn,
+                            user.username,
                             message,
                         )
 
-        print(f"Connection from {addr} closed")
-        conn.close()
+        print(f"Connection from {user.addr} closed")
+        user.conn.close()
 
     def start(self):
         """
@@ -129,16 +147,18 @@ class Server:
         """
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.bind((self.host, self.port))
-        print(f"Listening at {sock.getsockname()}")
         sock.listen()
+        print(f"Listening at {sock.getsockname()}")
 
         while True:
             try:
                 conn, addr = sock.accept()
 
                 # add client to list of connected clients
-                self.clients[addr] = {"conn": conn, "username": None}
+                user = User(conn, addr, None)
+                self.users.append(user)
+
                 # start a new thread to handle the client
-                threading.Thread(target=self.handle_client, args=(conn, addr)).start()
+                threading.Thread(target=self.handle_client, args=(user,)).start()
             except KeyboardInterrupt:
                 break
